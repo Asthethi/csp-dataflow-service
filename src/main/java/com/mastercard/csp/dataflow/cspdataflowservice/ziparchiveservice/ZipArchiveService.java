ad02120709795f6194784584c63aa24b11412256
asthethi <2024@Year!>
2026-07-12 03:39:11 +0530

    added archival of file implementation

--- src/main/java/com/mastercard/csp/dataflow/cspdataflowservice/ziparchiveservice/ZipArchiveService.java
+++ src/main/java/com/mastercard/csp/dataflow/cspdataflowservice/ziparchiveservice/ZipArchiveService.java
@@ -1,20 +1,47 @@
 package com.mastercard.csp.dataflow.cspdataflowservice.ziparchiveservice;
 
+import com.mastercard.csp.dataflow.cspdataflowservice.config.CspConfiguration;
 import com.mastercard.csp.dataflow.cspdataflowservice.model.FlowFileAttribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class ZipArchiveService {
 
-    public void process(FlowFileAttribute flowFileAttribute) {
-        try{
-            Files.deleteIfExists(flowFileAttribute.getPath());
-        } catch (Exception ex) {
-            System.out.println("Could not delete file: " + flowFileAttribute.getPath());
-        }
+    Logger LOG = LoggerFactory.getLogger(ZipArchiveService.class.getName());
 
-        System.out.println("Processing Zip Archive : Yet to be implemented");
+    private final CspConfiguration cspConfiguration;
+
+    public ZipArchiveService(CspConfiguration cspConfiguration) {
+        this.cspConfiguration = cspConfiguration;
+    }
+
+    public void process(FlowFileAttribute flowFileAttribute) {
+        try {
+
+            Path archiveDir = Paths.get(
+                    cspConfiguration.fileBasePath(),
+                    cspConfiguration.archiveFolder());
+
+            Files.createDirectories(archiveDir);
+
+            Path source = flowFileAttribute.getPath();
+            Path destination = archiveDir.resolve(source.getFileName());
+
+            LOG.info("Archiving {} -> {}", source, destination);
+
+            Files.move(source, destination, StandardCopyOption.REPLACE_EXISTING);
+
+            LOG.info("Successfully archived {}", source.getFileName());
+
+        } catch (Exception ex) {
+            LOG.error("Failed to archive file {}", flowFileAttribute.getPath(), ex);
+        }
     }
 }
